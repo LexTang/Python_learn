4ab2a3f429f80a74efd31515283049c3d3ea56c6
LexTang <lentang78@outlook.com>
2023-11-19 21:16:09 +0800

    231119

--- csp/202303_2.py
+++ csp/202303_2.py
@@ -1,28 +1,18 @@
 n, m, k = map(int, input().split())
-tc = {}
+size, lis = 0, [[]] * n
 for i in range(n):
     t, c = map(int, input().split())
-    if t in tc:
-        tc[t] += c
-    else:
-        tc[t] = c
-tc = [[key, value] for key, value in tc.items()]
-tc = sorted(tc, key=lambda x: x[0], reverse=True)
-i, end = 0, len(tc) - 1
-day = 0
-while i <= end:
-    t, c = tc[i]
-    if t <= k or m < c:
-        day = k if t <= k else t
+    lis[i] = [t, c]
+    size = max(t, size)
+tc = [0] * (size + 1)
+for t, c in lis:
+    tc[t] += c
+i = size
+while i > k:
+    c = tc[i]
+    if m < c:
         break
-    if i == end:
-        day = max(k, day - (m // c))
-        break
-    if m > (t - tc[i + 1][0]) * c:
-        m -= (t - tc[i + 1][0]) * c
-        tc[i + 1][1] += c
-        i += 1
-        continue
-    tc[i][0] -= 1
     m -= c
-print(day)
+    i -= 1
+    tc[i] += c
+print(i)
