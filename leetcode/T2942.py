fca3eef4f28a1f5ee817b3966d161e0b67c46207
LexTang <lentang78@outlook.com>
2023-11-28 22:37:18 +0800

    生日快乐，小周

---
+++ leetcode/T2942.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        idx = []
+        for i in range(len(words)):
+            if x in words[i]:
+                idx.append(i)
+        return idx
